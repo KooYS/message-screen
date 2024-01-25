355a96364f5516fa4823f23c8c5708fdfad539de
KooYS <tlfkthsl42@gmail.com>
2024-01-25 15:41:30 +0900

    feat: msg input change 2 textarea

--- src/app/message/page.tsx
+++ src/app/message/page.tsx
@@ -57,16 +57,17 @@ export default function Home() {
           className="h-full w-full fixed top-0 left-0 bg-black opacity-50"
         ></div>
       )}
-      <div className="flex items-center justify-between p-4 border-t border-gray-200 h-full">
-        <input
+      <div className="flex flex-col justify-center gap-3 p-4 border-t border-gray-200 h-full">
+        <textarea
+          rows={7}
           placeholder="메세지를 입력하세요..."
           id="message"
-          className="flex-1 p-2 border rounded-md focus:outline-none focus:border-blue-500"
-          onKeyDown={(e: React.KeyboardEvent) => {
-            if (e.key === 'Enter') {
-              send(); // 작성한 댓글 post 요청하는 함수
-            }
-          }}
+          className="p-2 border rounded-md focus:outline-none focus:border-blue-500"
+          // onKeyDown={(e: React.KeyboardEvent) => {
+          //   if (e.key === 'Enter') {
+          //     send(); // 작성한 댓글 post 요청하는 함수
+          //   }
+          // }}
         />
         <button
           className="ml-4 px-4 py-2 bg-blue-500 text-white fold:text-xs fold:ml-2 mobile:ml-4 mobile:text-base rounded-md hover:bg-blue-600 focus:outline-none"
